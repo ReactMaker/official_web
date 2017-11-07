2205a3145481934e7ede13124be86f56ac19ed62
Andy Tsai <bbandydd@gmail.com>
2017-11-08 00:17:38 +0800

    add react-scroll

--- src/containers/Home/components/HomeTitle/HomeTitle.js
+++ src/containers/Home/components/HomeTitle/HomeTitle.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-// import Particles from 'react-particles-js';
+import Particles from 'react-particles-js';
 import FontAwesome from 'react-fontawesome';
-// import particleSetting from './particle.json';
+import Scroll from 'react-scroll';
+import particleSetting from './particle.json';
+
+const Element = Scroll.Element;
+const scroll = Scroll.animateScroll;
 
 export default class HomeTitle extends Component {
-  render() {
-    // const particleStyle = {
-    //   position: 'absolute',
-    //   top: 0,
-    //   left: 0,
-    // };
+  scrollDown = () => {
+    scroll.scrollTo(window.innerHeight);
+  }
 
-    // <Particles width="100%" height="100%" style={particleStyle} params={particleSetting} /> 先註解掉
+  render() {
+    const particleStyle = {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    };
 
     return (
       <div className="section" id="homeTitle">
+        <Particles width="100%" height="100%" style={particleStyle} params={particleSetting} />
         <div className="welcome">
           <h3>Hello, Wellcome to</h3>
           <h1>ReactMaker</h1>
         </div>
-        <div className="scrollDown bounce">
+        <div onClick={this.scrollDown} className="scrollDown bounce">
           <FontAwesome name="chevron-down" />
         </div>
       </div>
