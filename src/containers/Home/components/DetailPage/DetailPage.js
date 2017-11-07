ee19f2ff062cb3200c957486c2adb61e0426339f
Andy Tsai <bbandydd@gmail.com>
2017-11-08 00:45:29 +0800

    scroll detail page hide menu

--- src/containers/Home/components/DetailPage/DetailPage.js
+++ src/containers/Home/components/DetailPage/DetailPage.js
@@ -10,12 +10,24 @@ export default class DetailPage extends Component {
     match: PropTypes.object,
   }
 
+  componentDidMount() {
+    this.page.addEventListener('scroll', () => {
+      const menuDom = document.getElementsByClassName('menu')[0];
+
+      if (this.page.scrollTop > 0) {
+        menuDom.classList.add('opacity');
+      } else {
+        menuDom.classList.remove('opacity');
+      }
+    });
+  }
+
   render() {
     const { type } = this.props.match.params;
     const matchObject = webJson.find(item => item.key === type);
 
     return (
-      <div className="detailPage">
+      <div ref={page => (this.page = page)} className="detailPage">
         <p>數量：{matchObject.dataList.length}</p>
         <div className="detailContainer">
           {
